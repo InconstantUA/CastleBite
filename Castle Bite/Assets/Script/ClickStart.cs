93b56b7b4a2811bea5fab42c6f74afae80c355fd
InconstantUA <38923827+InconstantUA@users.noreply.github.com>
2018-05-08 01:21:26 +0200

    playing with text

---
+++ Castle Bite/Assets/Script/ClickStart.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ClickStart : MonoBehaviour {
+    public Color newHighlightColor;
+    public string newSceneName;
+
+    //private void SetCollider()
+    //{
+    //    Text text = GetComponent<Text>();
+    //    if(text)
+    //    {
+    //        Rect box = text.GetPixelAdjustedRect();
+    //        BoxCollider boxCollider = (BoxCollider)gameObject.AddComponent(typeof(BoxCollider));
+    //        boxCollider.isTrigger = true;
+    //        Debug.Log("boxX = " + box.width + " boxY = " + box.height);
+    //        Debug.Log("text " + text.text);
+    //        boxCollider.size = new Vector3(box.width, box.height, 1);
+    //    }
+    //}
+
+    //void Start()
+    //{
+    //    SetCollider();
+    //}
+
+    private void OnMouseOver()
+    {
+        GetComponent<Text>().color = newHighlightColor;
+    }
+
+    private void OnMouseUp()
+    {
+        SceneManager.LoadScene(newSceneName);
+    }
+
+
+}
