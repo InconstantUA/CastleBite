0a19545716cdd1df42c3f161deb30974af2a0868
InconstantUA <38923827+InconstantUA@users.noreply.github.com>
2018-05-10 16:13:08 +0200

    Added font size graphic option

---
+++ Castle Bite/Assets/Script/MenuOptionsVideoFontSizeControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuOptionsVideoFontSizeControl : MonoBehaviour, IDragHandler {
+    Text txt;
+    Slider sld;
+
+    private void Start()
+    {
+        txt = transform.parent.gameObject.GetComponentInChildren<Text>();
+        sld = gameObject.GetComponent<Slider>();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        txt.text = sld.value.ToString();
+        txt.fontSize = (int)sld.value;
+    }
+}
