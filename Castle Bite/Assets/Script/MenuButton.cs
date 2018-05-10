30491b5a92165df02b875ff289f0bfc82df39516
InconstantUA <38923827+InconstantUA@users.noreply.github.com>
2018-05-10 13:05:49 +0200

    Added Options submenu and improved keyboard and mouse controls

--- Castle Bite/Assets/Script/MenuButton.cs
+++ Castle Bite/Assets/Script/MenuButton.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 // Use the same colors as defined for the button but for the text
 // Button in our case is not visible, only text is visible
@@ -10,9 +11,7 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Button))]
 public class MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
 {
-    Button[] menuObjList;
     Text txt;
-    // Color baseColor;
     Button btn;
     bool interactableDelay;
     Color tmpColor;
@@ -28,38 +27,38 @@ public class MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
 
     void Start()
     {
-        // get all other menus to be able to disable them later
-        menuObjList = transform.parent.GetComponentsInChildren<Button>();
         // init text object
         txt = GetComponentInChildren<Text>();
         // baseColor = txt.color;
         btn = gameObject.GetComponent<Button>();
-        interactableDelay = btn.interactable;
+        // interactableDelay = btn.interactable;
         // use the same color as set for button normal color
         // otherwise we will have to remember to change both values
         // because at the game start text will not have the same color as button
-        tmpColor = btn.colors.normalColor;
-        tmpColor.a = 1;
-        txt.color = tmpColor;
+        // Update: if we do this, then it causes Start button not to be highlighted at the start
+        // so it cannot be used by keyboard
+        // tmpColor = btn.colors.normalColor;
+        // tmpColor.a = 1;
+        // txt.color = tmpColor;
         currentState = States.Unknown;
     }
 
     void Update()
     {
-        if (btn.interactable != interactableDelay)
-        {
-            if (btn.interactable)
-            {
-                tmpColor = btn.colors.normalColor;
-            }
-            else
-            {
-                tmpColor = btn.colors.disabledColor;
-            }
-            tmpColor.a = 1;
-            txt.color = tmpColor;
-        }
-        interactableDelay = btn.interactable;
+        //if (btn.interactable != interactableDelay)
+        //{
+        //    if (btn.interactable)
+        //    {
+        //        tmpColor = btn.colors.normalColor;
+        //    }
+        //    else
+        //    {
+        //        tmpColor = btn.colors.disabledColor;
+        //    }
+        //    tmpColor.a = 1;
+        //    txt.color = tmpColor;
+        //}
+        //interactableDelay = btn.interactable;
         // enable mouse on its move, if it was disabled before
         if ((Input.GetAxis("Mouse X") != 0) || (Input.GetAxis("Mouse Y") != 0) & (! Cursor.visible))
         {
@@ -85,6 +84,7 @@ public class MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
         }
         tmpColor.a = 1;
         txt.color = tmpColor;
+        Debug.Log("highlight " + btn.name + "button");
     }
 
     void SetPressedStatus()
@@ -104,7 +104,7 @@ public class MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
     void DimmAllOtherMenus()
     {
         // to make it easier dimm just all menus
-        foreach (Button otherButton in menuObjList)
+        foreach (Button otherButton in transform.parent.GetComponentsInChildren<Button>())
         {
             if (otherButton.interactable)
             {
@@ -116,7 +116,7 @@ public class MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
             }
             tmpColor.a = 1;
             otherButton.GetComponentInChildren<Text>().color = tmpColor;
-            Debug.Log("dimm " + otherButton.name + "button");
+            // Debug.Log("dimm " + otherButton.name + "button");
         }
     }
 
@@ -139,10 +139,91 @@ public class MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        // set state
-        currentState = States.Highlighted;
-        SetHighlightedStatus();
+        ActOnClick();
     }
+
+    #region OnClick
+
+    Button[] menuBtnsList; // required for MenuKeyboardControl.cs
+
+    void ActOnClick()
+    {
+        // act depending on the parent menu
+        string currActiveMenuName = btn.transform.parent.name;
+        switch (currActiveMenuName)
+        {
+            case "MainMenuPanel":
+                OnMainMenuClick();
+                break;
+            case "OptionsSubmenuL2Panel":
+                OnOptionsSubmenuL2Click();
+                break;
+            default:
+                Debug.Log("Error: unknown selected menu name [" + currActiveMenuName + "]");
+                break;
+        }
+    }
+
+    void SetActiveMenuTo(string mName)
+    {
+        // Disable old menu and activate new menu
+        btn.transform.parent.gameObject.SetActive(false);
+        GameObject newMenu = btn.transform.root.Find(mName).gameObject;
+        newMenu.SetActive(true);
+        // Update list of all buttons in the menu - required for keyboard control script
+        menuBtnsList = newMenu.GetComponentsInChildren<Button>();
+    }
+
+    void OnMainMenuClick()
+    {
+        string selectedMBtnName = btn.name;
+        switch (selectedMBtnName)
+        {
+            case "Start":
+                SceneManager.LoadScene(1);
+                break;
+            case "Options":
+                SetActiveMenuTo("OptionsSubmenuL2Panel");
+                break;
+            case "Quit":
+                Application.Quit();
+                break;
+            default:
+                Debug.Log("Error: unknown selected button name [" + selectedMBtnName + "]");
+                break;
+        }
+    }
+
+    void OnOptionsSubmenuL2Click()
+    {
+        string selectedMBtnName = btn.name;
+        switch (selectedMBtnName)
+        {
+            case "ReturnToTheMainMenu":
+                SetActiveMenuTo("MainMenuPanel");
+                //// disable Options sub-menu and activate Main menu
+                //transform.parent.gameObject.SetActive(false);
+                //transform.root.Find("MainMenuPanel").gameObject.SetActive(true);
+                break;
+            case "Game":
+                // activate Game sub-options
+                break;
+            case "Video":
+                // activate Video sub-options
+                break;
+            case "Audio":
+                // activate Audio sub-options
+                break;
+            case "KeyBindings":
+                // activate KeyBindings sub-options
+                break;
+            default:
+                Debug.Log("Error: unknown selected button name [" + selectedMBtnName + "]");
+                break;
+        }
+    }
+
+    #endregion OnClick
 
     public void OnPointerExit(PointerEventData eventData)
     {
@@ -159,5 +240,4 @@ public class MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
         //tmpColor.a = 1;
         //txt.color = tmpColor;
     }
-
 }
--- Castle Bite/Assets/Script/MenuKeyboardControl.cs
+++ Castle Bite/Assets/Script/MenuKeyboardControl.cs
@@ -2,19 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuKeyboardControl : MonoBehaviour {
-    Button[] menuObjList;
-    int currentlySelectedMenuID;
+    Button btn; // currently selected button
+    int currSelctdBtnID;
     int previouslySelectedMenuID;
     Color tmpColor;
 
     // Use this for initialization
-    void Start () {
+    void Start() {
         // Init list of all buttons in the menu
-        menuObjList = GetComponentsInChildren<Button>();
+        menuBtnsList = GetComponentsInChildren<Button>();
         // pre-select first element in the menu
-        currentlySelectedMenuID = 0;
+        currSelctdBtnID = 0;
         previouslySelectedMenuID = 0;
         HighlightSelectedMenu();
     }
@@ -23,95 +24,329 @@ public class MenuKeyboardControl : MonoBehaviour {
     {
         if (Input.GetKeyDown(KeyCode.DownArrow) == true)
         {
+            // switch to the other menu
             // audio.PlayOneShot(blip);
             // Disable cursor, to not conflict with keyboard input
             // If cursor was enabled, then we should verify if highlighted menu has not changed
             // and we cannot rely on previoiusly set previouslySelectedMenuID, 
-            // that is why we each time get it from scratch using GetCurrentlySelectedMenuID();
+            // that is why we each time get it from scratch using GetCurrentlySelectedBtnID();
             Cursor.visible = false;
-            currentlySelectedMenuID = GetCurrentlySelectedMenuID();
-            previouslySelectedMenuID = currentlySelectedMenuID;
-            if (currentlySelectedMenuID < menuObjList.Length - 1)
+            currSelctdBtnID = GetCurrentlySelectedBtnID();
+            previouslySelectedMenuID = currSelctdBtnID;
+            if (currSelctdBtnID < menuBtnsList.Length - 1)
             {
-                currentlySelectedMenuID += 1;
+                currSelctdBtnID += 1;
             }
             else
             {
-                currentlySelectedMenuID = 0;
+                currSelctdBtnID = 0;
             }
             DimmPreviouslySelectedMenu();
             HighlightSelectedMenu();
         }
         if (Input.GetKeyDown(KeyCode.UpArrow) == true)
         {
+            // switch to the other menu
             // audio.PlayOneShot(blip);
             Cursor.visible = false;
-            currentlySelectedMenuID = GetCurrentlySelectedMenuID();
-            previouslySelectedMenuID = currentlySelectedMenuID;
-            if (currentlySelectedMenuID > 0)
+            currSelctdBtnID = GetCurrentlySelectedBtnID();
+            previouslySelectedMenuID = currSelctdBtnID;
+            if (currSelctdBtnID > 0)
             {
-                currentlySelectedMenuID -= 1;
+                currSelctdBtnID -= 1;
             }
             else
             {
-                currentlySelectedMenuID = menuObjList.Length - 1;
+                currSelctdBtnID = menuBtnsList.Length - 1;
             }
             DimmPreviouslySelectedMenu();
             HighlightSelectedMenu();
         }
+        if (Input.GetKeyDown(KeyCode.Return) == true)
+        {
+            SetPressedStatus();
+        }
+        if (Input.GetKeyUp(KeyCode.Return) == true)
+        {
+            // update btn object, which later will be used in act on click
+            btn = menuBtnsList[currSelctdBtnID];
+            ActOnClick();
+        }
     }
 
-    int GetCurrentlySelectedMenuID()
+    #region OnClick
+
+    Button[] menuBtnsList; // required for MenuKeyboardControl.cs
+
+    void ActOnClick()
+    {
+        // act depending on the parent menu
+        string currActiveMenuName = btn.transform.parent.name;
+        switch (currActiveMenuName)
+        {
+            case "MainMenuPanel":
+                OnMainMenuClick();
+                break;
+            case "OptionsSubmenuL2Panel":
+                OnOptionsSubmenuL2Click();
+                break;
+            case "OptionsGameSubmenuL3Panel":
+                OptionsGameSubmenuL3PanelClick();
+                break;
+            default:
+                Debug.Log("Error: unknown selected menu name [" + currActiveMenuName + "]");
+                break;
+        }
+    }
+
+    void ResetAllMenuButtonsToNormal()
+    {
+        foreach (Button tmpBtn in menuBtnsList)
+        {
+            if (tmpBtn.interactable)
+            {
+                tmpColor = tmpBtn.colors.normalColor;
+            }
+            else
+            {
+                tmpColor = tmpBtn.colors.disabledColor;
+            }
+            tmpColor.a = 1;
+            tmpBtn.GetComponentInChildren<Text>().color = tmpColor;
+            // Debug.Log("dimm " + tmpBtn.name + "button");
+        }
+
+    }
+
+    void SetActiveMenuTo(string mName, int btnID = -1)
+    {
+        // Disable old menu and activate new menu
+        btn.transform.parent.gameObject.SetActive(false);
+        GameObject newMenu = btn.transform.root.Find(mName).gameObject;
+        newMenu.SetActive(true);
+        // Update list of all buttons in the menu - required for keyboard control script
+        menuBtnsList = newMenu.GetComponentsInChildren<Button>();
+        #region Keyboard-specific
+        // Set currently selected button id and highligh it
+        if ((btnID >= 0) && (btnID < menuBtnsList.Length))
+        {
+            // reset all menus, because they might be still highlighed from previous activity
+            ResetAllMenuButtonsToNormal();
+            currSelctdBtnID = btnID;
+        }
+        else
+        {
+            // If btnID is not provided, or it is invalid then keep previously pressed button
+            currSelctdBtnID = GetCurrentlyPressedBtnID();
+        }
+        HighlightSelectedMenu();
+        #endregion Keyboard-specific
+    }
+
+    void OnMainMenuClick()
+    {
+        string selectedMBtnName = btn.name;
+        switch (selectedMBtnName)
+        {
+            case "Start":
+                SceneManager.LoadScene(1);
+                break;
+            case "Options":
+                SetActiveMenuTo("OptionsSubmenuL2Panel", 1);
+                break;
+            case "Quit":
+                Application.Quit();
+                break;
+            default:
+                Debug.Log("Error: unknown selected button name [" + selectedMBtnName + "]");
+                break;
+        }
+    }
+
+    void OnOptionsSubmenuL2Click()
+    {
+        string selectedMBtnName = btn.name;
+        switch (selectedMBtnName)
+        {
+            case "ReturnToTheMainMenu":
+                SetActiveMenuTo("MainMenuPanel");
+                //// disable Options sub-menu and activate Main menu
+                //transform.parent.gameObject.SetActive(false);
+                //transform.root.Find("MainMenuPanel").gameObject.SetActive(true);
+                break;
+            case "Game":
+                SetActiveMenuTo("OptionsGameSubmenuL3Panel", 1);
+                break;
+            case "Video":
+                // activate Video sub-options
+                break;
+            case "Audio":
+                // activate Audio sub-options
+                break;
+            case "KeyBindings":
+                // activate KeyBindings sub-options
+                break;
+            default:
+                Debug.Log("Error: unknown selected button name [" + selectedMBtnName + "]");
+                break;
+        }
+    }
+
+    void OptionsGameSubmenuL3PanelClick()
+    {
+        string selectedMBtnName = btn.name;
+        switch (selectedMBtnName)
+        {
+            case "ReturnToTheOptionsMenu":
+                SetActiveMenuTo("OptionsSubmenuL2Panel");
+                //// disable Options sub-menu and activate Main menu
+                //transform.parent.gameObject.SetActive(false);
+                //transform.root.Find("MainMenuPanel").gameObject.SetActive(true);
+                break;
+            case "Autosave":
+                // Revert autosave setting on click
+                Text btnTxt = btn.GetComponentsInChildren<Text>()[1];
+                if (btnTxt.text == "Off")
+                {
+                    btnTxt.text = "On";
+                } else
+                {
+                    btnTxt.text = "Off";
+                }
+                break;
+            default:
+                Debug.Log("Error: unknown selected button name [" + selectedMBtnName + "]");
+                break;
+        }
+    }
+
+    #endregion OnClick
+
+    void SetPressedStatus()
+    {
+        if (menuBtnsList[currSelctdBtnID].interactable)
+        {
+            tmpColor = menuBtnsList[currSelctdBtnID].colors.pressedColor;
+        }
+        else
+        {
+            tmpColor = menuBtnsList[currSelctdBtnID].colors.disabledColor;
+        }
+        tmpColor.a = 1;
+        menuBtnsList[currSelctdBtnID].GetComponentInChildren<Text>().color = tmpColor;
+    }
+
+    //// take action according to the menu
+    //Cursor.visible = false;
+    //        currSelctdBtnID = GetCurrentlySelectedBtnID();
+    //string selectedMBtnName = menuBtnsList[currSelctdBtnID].name;
+    //        switch(selectedMBtnName)
+    //        {
+    //            case "Start":
+    //                SceneManager.LoadScene(1);
+    //                break;
+    //            case "Quit":
+    //                Application.Quit();
+    //                break;
+    //            default:
+    //                Debug.Log("Error: unknown selected button name [" + selectedMBtnName + "]");
+    //                break;
+    //        }
+
+    int GetCurrentlySelectedBtnID()
     {
         // it is possible that id has changed, because there was mouse activity
         // that is why I need to get active menu id set by mouse and verify if it is the same as for keyboard
-        int id;
+        int selectedBtnID = -1;
         // to get currently highlighted menu we compare its text color with highlighted color
-        for (id=0; id < menuObjList.Length; id++)
+        for (int id=0; id < menuBtnsList.Length; id++)
         {
             // do not compare alpha (transparancy) because it is different, because we set it to 1(255)
-            Debug.Log("Comparing " + menuObjList[id] + " menu");
-            Color btnHClr = menuObjList[id].colors.highlightedColor;
-            Color txtClr = menuObjList[id].GetComponentInChildren<Text>().color;
+            Debug.Log("Comparing " + menuBtnsList[id] + " menu");
+            Color btnHClr = menuBtnsList[id].colors.highlightedColor;
+            Color txtClr = menuBtnsList[id].GetComponentInChildren<Text>().color;
             if (((int)(txtClr.r * 1000) == (int)(btnHClr.r * 1000)) || ((int)(txtClr.g * 1000) == (int)(btnHClr.g * 1000)) || ((int)(txtClr.b * 1000) == (int)(btnHClr.b * 1000)))
             {
                 // found id
+                Debug.Log("Currently highlighted button is " + menuBtnsList[id].name);
+                selectedBtnID = id;
                 break;
             }
         }
-        Debug.Log("Currently highlighted menu object is " + menuObjList[id].name);
-        return id;
+        // Verfiy if we found the button id
+        if (selectedBtnID < 0)
+        {
+            // not found
+            // reset to 0
+            selectedBtnID = 0;
+        }
+        return selectedBtnID;
+    }
+
+    int GetCurrentlyPressedBtnID()
+    {
+        // it is possible that id has changed, because there was mouse activity
+        // that is why I need to get active menu id set by mouse and verify if it is the same as for keyboard
+        int pressedBtnID = -1;
+        // to get currently highlighted menu we compare its text color with highlighted color
+        for (int id = 0; id < menuBtnsList.Length; id++)
+        {
+            // do not compare alpha (transparancy) because it is different, because we set it to 1(255)
+            Debug.Log("Comparing " + menuBtnsList[id] + " menu");
+            Color btnHClr = menuBtnsList[id].colors.pressedColor;
+            Color txtClr = menuBtnsList[id].GetComponentInChildren<Text>().color;
+            if (((int)(txtClr.r * 1000) == (int)(btnHClr.r * 1000)) || ((int)(txtClr.g * 1000) == (int)(btnHClr.g * 1000)) || ((int)(txtClr.b * 1000) == (int)(btnHClr.b * 1000)))
+            {
+                // found id
+                Debug.Log("Currently highlighted button is " + menuBtnsList[id].name);
+                pressedBtnID = id;
+                break;
+            }
+        }
+        // Verfiy if we found the button id
+        if (pressedBtnID < 0)
+        {
+            // not found
+            // reset to 0
+            pressedBtnID = 0;
+        }
+        return pressedBtnID;
     }
 
     void DimmPreviouslySelectedMenu()
     {
         // Remove highlight for previously selected menu
-        if (menuObjList[previouslySelectedMenuID].interactable)
+        if (menuBtnsList[previouslySelectedMenuID].interactable)
         {
-            tmpColor = menuObjList[previouslySelectedMenuID].colors.normalColor;
+            tmpColor = menuBtnsList[previouslySelectedMenuID].colors.normalColor;
         }
         else
         {
-            tmpColor = menuObjList[previouslySelectedMenuID].colors.disabledColor;
+            tmpColor = menuBtnsList[previouslySelectedMenuID].colors.disabledColor;
         }
         tmpColor.a = 1;
-        menuObjList[previouslySelectedMenuID].GetComponentInChildren<Text>().color = tmpColor;
-        Debug.Log("Dimm menu object " + menuObjList[previouslySelectedMenuID].name);
+        menuBtnsList[previouslySelectedMenuID].GetComponentInChildren<Text>().color = tmpColor;
+        Debug.Log("Dimm button " + menuBtnsList[previouslySelectedMenuID].name);
     }
 
     void HighlightSelectedMenu()
     {
         // Highlight newly selected menu
-        if (menuObjList[currentlySelectedMenuID].interactable)
+        Debug.Log("id " + currSelctdBtnID);
+        foreach(Button dbg_btn in menuBtnsList)
         {
-            tmpColor = menuObjList[currentlySelectedMenuID].colors.highlightedColor;
+            Debug.Log("btn name " + dbg_btn.name);
+        }
+        if (menuBtnsList[currSelctdBtnID].interactable)
+        {
+            tmpColor = menuBtnsList[currSelctdBtnID].colors.highlightedColor;
         }
         else
         {
-            tmpColor = menuObjList[currentlySelectedMenuID].colors.disabledColor;
+            tmpColor = menuBtnsList[currSelctdBtnID].colors.disabledColor;
         }
         tmpColor.a = 1;
-        menuObjList[currentlySelectedMenuID].GetComponentInChildren<Text>().color = tmpColor;
-        Debug.Log("Highlight menu object " + menuObjList[currentlySelectedMenuID].name);
+        menuBtnsList[currSelctdBtnID].GetComponentInChildren<Text>().color = tmpColor;
+        Debug.Log("Highlight button " + menuBtnsList[currSelctdBtnID].name);
     }
 }
