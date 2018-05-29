d267c5e8aa94c88a5bfc1213b36c33f68914f394
InconstantUA <38923827+InconstantUA@users.noreply.github.com>
2018-05-30 00:09:52 +0200

    minor changes

--- Castle Bite/Assets/Script/UnitSlotDropHandler.cs
+++ Castle Bite/Assets/Script/UnitSlotDropHandler.cs
@@ -9,7 +9,6 @@ public class UnitSlotDropHandler : MonoBehaviour, IDropHandler
     {
         get
         {
-            // Debug.Log("Child count: " + transform.childCount.ToString());
             if (transform.childCount > 0)
             {
                 return transform.GetChild(0).gameObject;
@@ -23,9 +22,15 @@ public class UnitSlotDropHandler : MonoBehaviour, IDropHandler
         // drop unit if there is no other unit already present
         if (!unit)
         {
-            // enable hire unit button in the original parent cell and bring it to the front
-            UnitDragHandler.unitBeingDragged.transform.parent.parent.Find("HireUnitPnlBtn").gameObject.SetActive(true);
-            UnitDragHandler.unitBeingDragged.transform.parent.parent.Find("HireUnitPnlBtn").SetAsLastSibling();
+            PartyPanel.PanelMode panelMode = UnitDragHandler.unitBeingDragged.transform.parent.parent.parent.parent.GetComponent<PartyPanel>().GetPanelMode();
+            if (PartyPanel.PanelMode.Garnizon == panelMode)
+            {
+                // enable hire unit button in the original parent cell and bring it to the front
+                // and it is not wide
+                // todo: add not wide condition check
+                UnitDragHandler.unitBeingDragged.transform.parent.parent.Find("HireUnitPnlBtn").gameObject.SetActive(true);
+                UnitDragHandler.unitBeingDragged.transform.parent.parent.Find("HireUnitPnlBtn").SetAsLastSibling();
+            }
             // change parent of the dragged unit
             Debug.Log("Drop unit from " + UnitDragHandler.unitBeingDraggedParentTr.parent.gameObject.name);
             UnitDragHandler.unitBeingDragged.transform.SetParent(transform);
@@ -34,8 +39,14 @@ public class UnitSlotDropHandler : MonoBehaviour, IDropHandler
             UnitDragHandler.unitBeingDragged.transform.gameObject.GetComponent<RectTransform>().offsetMin = new Vector2(0, 0);
             // [ right - top ]
             UnitDragHandler.unitBeingDragged.transform.gameObject.GetComponent<RectTransform>().offsetMax = new Vector2(0, 0);
-            // disable hire unit button in the destination (this) cell
-            transform.parent.Find("HireUnitPnlBtn").gameObject.SetActive(false);
+            // disable hire unit button in the destination (this) cell, if it is garnizon panel
+            // structure 3PartyPanel-2Top/Middle/Bottom-1Left/Right/Wide-(this)UnitSlot
+            panelMode = transform.parent.parent.parent.GetComponent<PartyPanel>().GetPanelMode();
+            if (PartyPanel.PanelMode.Garnizon == panelMode)
+            {
+                // todo: add not wide condition check
+                transform.parent.Find("HireUnitPnlBtn").gameObject.SetActive(false);
+            }
             // trigger event
             // ExecuteEvents.ExecuteHierarchy<IHasChanged>(gameObject, null, (x, y) => x.HasChanged());
         }
